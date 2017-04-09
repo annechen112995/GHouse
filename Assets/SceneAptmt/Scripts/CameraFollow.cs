6c67823b7cbef77083196ee46129382cde808085
joswei <jsw033@ucsd.edu>
2017-04-08 22:05:45 -0700

    scrolling apartment scene

---
+++ Assets/SceneAptmt/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() {
+	}
+
+	public float speed = 1F;
+
+	//Update is called once per frame
+	void Update() {
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
+			Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+			transform.Translate(-touchDeltaPosition.x * speed * Time.deltaTime, 
+				0, 0);
+		}
+	}
+} 
