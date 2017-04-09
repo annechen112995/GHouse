835ba9fc24a7d1b75984a6b28eee61d28e0ece49
Westwood <annechen112995@gmail.com>
2017-04-09 00:31:08 -0700

    fixed fridgeclean

--- Assets/FridgeClean/Script/FoodBehaviourScript.cs
+++ Assets/FridgeClean/Script/FoodBehaviourScript.cs
@@ -8,30 +8,16 @@ public class FoodBehaviourScript : MonoBehaviour {
 	public GameObject Apple;			//Apple prefab
 	public GameObject AppleCore;		//AppleCore prefab
 	public float maxX;					//Max x spawn position
-	public float minX;					//Min x spawn position
-	private float height;
-	private float timeLimit;
-	private bool clicked;
-	private int numFood = 0;
-	public bool spawned = false;
+	public float minX;		 			//Min x spawn position
 
-	public void Trigger(float tl)
-	{
-		clicked = false;
-		timeLimit = tl;
-	}
+	public int count = 0;
+	public bool spawned = false;
 
 	void Start()
 	{
-		timeLimit = 3.0f;
-
-		if (numFood < 2 && spawned == false) 
-		{
-			spawned = true; 
-			numFood += 1;
-			//Start the spawn update
-			StartCoroutine("Spawn");
-		}
+		spawned = false;
+		//Start the spawn update
+		StartCoroutine("Spawn");
 	}
 
 	IEnumerator Spawn()
@@ -46,46 +32,15 @@ public class FoodBehaviourScript : MonoBehaviour {
 		{
 			//Spawn prefab is apple core
 			prefab = AppleCore;
-			AppleCore.SetActive (true);
 		}
-		StartCoroutine (MainLoop (prefab));
-		//Spawn prefab add random position
-		GameObject go = Instantiate (prefab, new Vector3 (Random.Range (-50, 60), Random.Range (-60, 50), 0),
-			Quaternion.Euler (0, 0, Random.Range (-100, 50))) as GameObject;
-		spawned = false;
-	}
 
-	// Main loop for the sprite.  Move up, then wait, then move down again. Simple.
-	private IEnumerator MainLoop(GameObject sprite)
-	{
-		yield return StartCoroutine(WaitForHit(sprite));
-	}
-
-	// Give the player a chance to click the fruit.
-	private IEnumerator WaitForHit(GameObject sprite)
-	{
-		float time = 0.0f;
-
-		while(!clicked && time < timeLimit)
-		{
-			time += Time.deltaTime;
-			yield return null;
-		}	
-	}
-
-	// Food has been clicked
-	public void Click(GameObject sprite)
-	{
-		clicked = true;
-
-		sprite.SetActive(false);
-	}
-
-	public bool Clicked
-	{
-		get
-		{
-			return clicked;	
+		if (count < 1 && spawned == false) {
+			count += 1;
+			spawned = true;
+			//Spawn prefab add random position
+			GameObject go = Instantiate (prefab, new Vector3 (Random.Range (-50, 60), Random.Range (-60, 50), 0),
+				Quaternion.Euler (0, 0, Random.Range (-100, 50))) as GameObject;
 		}
 	}
+
 }
