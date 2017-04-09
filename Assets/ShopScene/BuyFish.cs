3dd209679b4e93bdc767c28691456db1574f0557
joswei <jsw033@ucsd.edu>
2017-04-09 05:54:14 -0700

    .

---
+++ Assets/ShopScene/BuyFish.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuyFish : MonoBehaviour {
+
+	public void ClickFish() {
+		// Clicked fish, decrease number of coins
+		if ((PlayerPrefs.GetInt("coins") - 15) > 0) {
+			PlayerPrefs.SetInt("coins", PlayerPrefs.GetInt ("coins") - 15);
+		} else {
+			PlayerPrefs.SetInt("coins", 0);
+		}
+	}
+
+}
