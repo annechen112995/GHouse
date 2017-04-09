e6a008418358336fde7d846b490c87819f71724c
Chih-Chieh Lee <clee7@caltech.edu>
2017-04-09 00:09:09 -0700

    Laundry game - scene initialization and scripts

---
+++ Assets/Laundry/Scripts/Basket.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basket : MonoBehaviour {
+
+	public Sprite empty;
+	public Sprite semiFull;
+	public Sprite full;
+	public Sprite explode;
+
+	int numClothing;
+
+	// Update is called once per frame
+	void Update () {
+		
+		numClothing = GameObject.Find("GameManager").GetComponent<managerScript>().numDirtyCloth;
+
+		if (numClothing < 5) {
+			this.gameObject.GetComponent<SpriteRenderer>().sprite = empty;
+			return;
+		}
+		if (numClothing < 15) {
+			this.gameObject.GetComponent<SpriteRenderer>().sprite = semiFull;
+			return;
+		}
+		if (numClothing < 25) {
+			this.gameObject.GetComponent<SpriteRenderer>().sprite = full;
+			return;
+		}
+		else {
+			this.gameObject.GetComponent<SpriteRenderer>().sprite = explode;
+			return;
+		}
+	}
+}
---
+++ Assets/Laundry/Scripts/managerScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class managerScript : MonoBehaviour {
+	public int numDirtyCloth;
+	public int numHolding;
+
+	public Text numDirty;
+	public Text numHold;
+
+	float timer = 1f;
+	float delay = 1f;
+
+	// Use this for initialization
+	void Start () {
+		numDirtyCloth = 10;
+		numHolding = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		// update number of dirty cloth in basket (time increment)
+		timer -= Time.deltaTime;
+
+		if (timer <= 0) {
+			numDirtyCloth += 1;
+			timer = delay;
+		}
+
+		// update counter display
+		numDirty.text = "Dirty Clothes: " + numDirtyCloth;
+		numHold.text = "Holding: " + numHolding + "/ 10";
+		
+	}
+}
