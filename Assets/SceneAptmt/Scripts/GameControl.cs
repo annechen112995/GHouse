18ac2dff59671420264a1a998c77d191492bab50
joswei <jsw033@ucsd.edu>
2017-04-09 06:23:31 -0700

    .

--- Assets/SceneAptmt/Scripts/GameControl.cs
+++ Assets/SceneAptmt/Scripts/GameControl.cs
@@ -19,12 +19,12 @@ public class GameControl : MonoBehaviour {
 	void OnGUI() {
 		numCoins = PlayerPrefs.GetInt("coins");
 		guiStyle.fontSize = 50; //change the font size
-		if (SceneManager.GetActiveScene().name == "Scene1") {
+		if (SceneManager.GetActiveScene().name == "Scene1" || SceneManager.GetActiveScene().name == "ShopScene") {
 			if (!aTexture) {
 				return;
 			}
 			GUI.DrawTexture(new Rect(0, 15, 70, 70), aTexture, ScaleMode.ScaleToFit);
-			GUI.Label (new Rect(50, 18, 200, 50), numCoins.ToString(), guiStyle);
+			GUI.Label(new Rect(50, 18, 200, 50), numCoins.ToString(), guiStyle);
 		}
 	}
 
--- Assets/ShopScene/BuyFish.cs
+++ Assets/ShopScene/BuyFish.cs
@@ -1,16 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BuyFish : MonoBehaviour {
+
+	public Texture aTexture;
+	private int numCoins = -1;
+
+	private GUIStyle guiStyle = new GUIStyle(); //create a new variable
+	void Start() {
+		numCoins = PlayerPrefs.GetInt("coins");
+	}
 
 	public void ClickFish() {
 		// Clicked fish, decrease number of coins
 		if ((PlayerPrefs.GetInt("coins") - 15) > 0) {
 			PlayerPrefs.SetInt("coins", PlayerPrefs.GetInt ("coins") - 15);
-		} else {
-			PlayerPrefs.SetInt("coins", 0);
 		}
+	}
+
+	void OnGUI() {
+		numCoins = PlayerPrefs.GetInt("coins");
+		guiStyle.fontSize = 50; //change the font size
+		if (SceneManager.GetActiveScene().name == "Scene1" || SceneManager.GetActiveScene().name == "ShopScene") {
+			if (!aTexture) {
+				return;
+			}
+			GUI.DrawTexture(new Rect(0, 15, 70, 70), aTexture, ScaleMode.ScaleToFit);
+			GUI.Label(new Rect(55, 18, 200, 50), numCoins.ToString(), guiStyle);
+		}
+	}
+
+	void Update() {
+		OnGUI();
 	}
 
 }
