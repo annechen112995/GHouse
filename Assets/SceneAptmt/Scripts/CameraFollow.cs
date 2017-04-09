20b187a8929a0028b66a975bdbcc9d1323bfa3e4
joswei <jsw033@ucsd.edu>
2017-04-09 00:17:02 -0700

    click fridge and back button for fridge

--- Assets/SceneAptmt/Scripts/CameraFollow.cs
+++ Assets/SceneAptmt/Scripts/CameraFollow.cs
@@ -12,15 +12,14 @@ public class CameraFollow : MonoBehaviour {
 
 	//Update is called once per frame
 	void FixedUpdate() {
-		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
-			Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Moved) {
+			Vector2 touchDeltaPosition = Input.GetTouch (0).deltaPosition;
 			transform.Translate (-touchDeltaPosition.x * speed * Time.deltaTime, 
 				0, 0);
-			transform.position = new Vector3(
-				Mathf.Clamp(transform.position.x, 0, x_max),
-				Mathf.Clamp(transform.position.y, 0, 0),
-				Mathf.Clamp(transform.position.z, -10, -10));
+			transform.position = new Vector3 (
+				Mathf.Clamp (transform.position.x, 0, x_max),
+				Mathf.Clamp (transform.position.y, 0, 0),
+				Mathf.Clamp (transform.position.z, -10, -10));
 		}
-
 	}
-} 
+}
