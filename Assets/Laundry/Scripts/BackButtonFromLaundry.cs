53a5d2fb73c3f21636b39c5fb834f6c0bd8b0dd1
joswei <jsw033@ucsd.edu>
2017-04-09 01:18:01 -0700

    coins

---
+++ Assets/Laundry/Scripts/BackButtonFromLaundry.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackButtonFromLaundry : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// From the Apartment Scene
+		// If Android's Back Button is pressed, go back to the Previous Scene (Start Scene)
+		if (Input.GetKeyUp (KeyCode.Escape)) {
+			SceneManager.LoadScene ("Scene1");
+		}
+	}
+}
