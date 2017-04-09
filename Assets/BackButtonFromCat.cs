a8eb8804d0229ae58c4102ff7c21986d2633b2eb
joswei <jsw033@ucsd.edu>
2017-04-09 07:10:20 -0700

    umbrella back

---
+++ Assets/BackButtonFromCat.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackButtonFromCat : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		// From the Apartment Scene
+		// If Android's Back Button is pressed, go back to the Previous Scene (Start Scene)
+		if (Input.GetKeyUp (KeyCode.Escape)) {
+			Destroy(this.gameObject);
+			SceneManager.LoadScene ("Scene1");
+		}
+	}
+}
